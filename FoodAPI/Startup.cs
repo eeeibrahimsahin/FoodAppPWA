bd59024831b9af24a8de2279ab13f1b78af51860
eeeibrahimsahin <eeeibrahimsahin@gmail.com>
2021-03-10 09:10:07 +0100

    Add project files.

---
+++ FoodAPI/Startup.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FoodAPI", Version = "v1" });
+            });
+
+            services.AddDbContext<FoodDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //Add Cors
+            services.AddCors(o => o.AddPolicy("Policy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FoodAPI v1"));
+            }
+
+            app.UseCors("Policy");
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ FoodAppPWA/Services/FoodDataService.cs
@@ -0,0 +1,64 @@
+﻿using FoodDataApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FoodAppPWA.Services
+{
+    public class FoodDataService : IFoodDataService
+    {
+        private readonly HttpClient httpClient;
+        public FoodDataService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        string baseUrl = "https://localhost:44396/";
+
+        public async Task<Food> AddFood(Food food)
+        {
+            var foodJson =
+               new StringContent(JsonSerializer.Serialize(food), Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync("api/foods", foodJson);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await JsonSerializer.DeserializeAsync<Food>(await response.Content.ReadAsStreamAsync());
+            }
+
+            return null;
+        }
+
+        public async Task DeleteFood(int foodId)
+        {
+            await httpClient.DeleteAsync($"api/foods/{foodId}");
+        }
+
+        public async Task<IEnumerable<Food>> GetAllFood()
+        {
+            return await JsonSerializer.DeserializeAsync<IEnumerable<Food>>
+            (await httpClient.GetStreamAsync($"api/foods"), new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+
+        public async Task<Food> GetFoodDetails(int foodId)
+        {
+            return await JsonSerializer.DeserializeAsync<Food>
+               (await httpClient.GetStreamAsync($"{baseUrl}api/foods/{foodId}"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+
+        public async Task UpdateFood(Food food)
+        {
+            var foodJson =
+                new StringContent(JsonSerializer.Serialize(food), Encoding.UTF8, "application/json");
+
+            await httpClient.PutAsync("api/foods", foodJson);
+        }
+    }
+}
